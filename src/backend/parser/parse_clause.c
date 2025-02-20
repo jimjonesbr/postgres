93ea0e1b07763bca7ffb1d4cf88cc380640604cb
Jim Jones <jim.jones@uni-muenster.de>
2025-02-20 09:58:34 +0100

    Add XMLNamespaces option to XMLElement
    This patch adds the scoped option XMLNamespaces to XMLElement,
    as described in ISO/IEC 9075-14:2023, 11.2 XML lexically scoped
    options:
    
    xmlnamespaces(uri AS prefix, ...)
    xmlnamespaces(DEFAULT uri, ...)
    xmlnamespaces(NO DEFAULT, ...)
    
    * prefix:         Namespace's prefix.
    * uri:            Namespace's URI.
    * DEFAULT prefix: Specifies the DEFAULT namespace to use within
    the scope of a namespace declaration.
    * NO DEFAULT:     Specifies that NO DEFAULT namespace is to be
    used within the scope of a namespace declaration.
    
    == Examples ==
    
    SELECT xmlelement(NAME "foo", xmlnamespaces('http:/x.y' AS bar));
              xmlelement
    ------------------------------
     <foo xmlns:bar="http:/x.y"/>
    
    SELECT xmlelement(NAME "foo", xmlnamespaces(DEFAULT 'http:/x.y'));
            xmlelement
    --------------------------
     <foo xmlns="http:/x.y"/>
    
     SELECT xmlelement(NAME "foo", xmlnamespaces(NO DEFAULT));
       xmlelement
    -----------------

--- src/backend/parser/parse_clause.c
+++ src/backend/parser/parse_clause.c
@@ -842,7 +842,12 @@ transformRangeTableFunc(ParseState *pstate, RangeTableFunc *rtf)
 			Node	   *ns_uri;
 
 			Assert(IsA(r, ResTarget));
-			ns_uri = transformExpr(pstate, r->val, EXPR_KIND_FROM_FUNCTION);
+			/* Create an empty String for NO DEFAULT namespaces */
+			if (!r->name && !r->val)
+				ns_uri = transformExpr(pstate, makeStringConst("", r->location), EXPR_KIND_FROM_FUNCTION);
+			else
+				ns_uri = transformExpr(pstate, r->val, EXPR_KIND_FROM_FUNCTION);
+
 			ns_uri = coerce_to_specific_type(pstate, ns_uri,
 											 TEXTOID, constructName);
 			assign_expr_collations(pstate, ns_uri);
